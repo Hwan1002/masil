274332fbf1ed7ca678ecea84907fa685f7b238b9
Fk1128 <ohyhsf@naver.com>
2025-02-05 17:19:52 +0900

    file저장경로
    file저장 경로 url로 변경

--- masil-spring/masil/src/main/java/project/masil/common/FileUploadUtil.java
+++ masil-spring/masil/src/main/java/project/masil/common/FileUploadUtil.java
@@ -40,7 +40,7 @@ public class FileUploadUtil {
 			File destinationFile = new File(fullUploadDir + File.separator + fileName);
 			file.transferTo(destinationFile);
 
-			return "uploads/" + subDir + "/" + fileName; // 상대 경로 반환
+			return "http://localhost:8080/uploads/" + subDir + "/" + fileName; // 상대 경로 반환
 		} catch (IOException e) {
 			throw new RuntimeException("파일 저장 중 오류가 발생했습니다.", e);
 		}
