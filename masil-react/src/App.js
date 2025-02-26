388fb7a3dbfa7750959584d364d6cfeba93e45d6
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2025-02-26 09:31:38 +0900

    Update App.js

--- masil-react/src/App.js
+++ masil-react/src/App.js
@@ -11,7 +11,7 @@ import UserFindPwd from './screen/UserFindPwd';
 import PostRegist from './screen/PostRegist';
 import RentalItem from "./screen/RentalItem";
 
-
+//테스트
 function App() {
   function test = () => {
   console.log("callback");
