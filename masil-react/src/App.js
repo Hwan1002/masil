587aa36aaba27ea330363bb48b82961ea3be9aab
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2025-02-27 09:11:55 +0900

    Update App.js

--- masil-react/src/App.js
+++ masil-react/src/App.js
@@ -10,7 +10,6 @@ import UserFindId from './screen/UserFindId';
 import UserFindPwd from './screen/UserFindPwd';
 import PostRegist from './screen/PostRegist';
 import RentalItem from "./screen/RentalItem";
-//잔디심기금잔디
 
 function App() {
   return (
