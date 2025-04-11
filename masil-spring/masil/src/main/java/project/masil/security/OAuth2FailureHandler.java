96c3ba618a620c19eed90801200bf68c9985e193
Fk1128 <ohyhsf@naver.com>
2025-04-11 15:35:02 +0900

    commit

--- masil-spring/masil/src/main/java/project/masil/security/OAuth2FailureHandler.java
+++ masil-spring/masil/src/main/java/project/masil/security/OAuth2FailureHandler.java
@@ -1,17 +1,10 @@
 package project.masil.security;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class OAuth2FailureHandler implements AuthenticationFailureHandler{
-    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationFailure(
@@ -28,18 +20,16 @@ public class OAuth2FailureHandler implements AuthenticationFailureHandler{
         HttpServletResponse response,
         AuthenticationException exception
     ) throws IOException {
-        // 응답 설정
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-
-        // 에러 메시지 생성
-        Map<String, String> errorData = new HashMap<>();
-        errorData.put("message", exception.getMessage());
-
-        // JSON 변환 및 전송
-        response.getWriter().write(
-            objectMapper.writeValueAsString(errorData)
+        String errorMessage = exception.getMessage();
+        String script = String.format(
+            "<script>" +
+            "window.opener.postMessage({ success: false, error: '%s' }, '%s');" +
+            "window.close();" +
+            "</script>",
+            errorMessage, "http://localhost:3000"
         );
+
+        response.setContentType("text/html; charset=UTF-8");
+        response.getWriter().print(script);
     }
 }
