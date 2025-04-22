791eddedc653a1450af398b45169e8c0721cdc9a
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2025-04-22 16:56:51 +0900

    Update App.js

--- masil-react/src/App.js
+++ masil-react/src/App.js
@@ -13,7 +13,7 @@ import UserFindPwd from "./screen/UserFindPwd";
 import PostRegist from "./screen/PostRegist";
 import RentalItem from "./screen/RentalItem";
 import SelectedRentalItem from "./screen/SelectedRentalItem";
-
+//wkseltlarl tlqkf 4qnsskadkTek dj
 function App() {
   const {
     isModalOpen,
