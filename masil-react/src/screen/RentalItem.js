59028b17c3d8445332f3d36292c483a810b0509d
dongugchoi <ehddnr7321@gmail.com>
2025-04-22 17:40:10 +0900

    add

--- masil-react/src/screen/RentalItem.js
+++ masil-react/src/screen/RentalItem.js
@@ -19,6 +19,7 @@ const RentalItem = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`http://localhost:9090/post`);
+        console.log(response)
         if (response) setItems(response.data);
       } catch (error) {
         console.error("데이터 불러오기 실패:", error);
