6d9973e7ef0d4b5f0e37bd61a374454205a29f36
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2025-12-29 09:03:17 +0900

    Update App.js

--- masil-react/src/App.js
+++ masil-react/src/App.js
@@ -10,7 +10,7 @@ import UserFindId from './screen/UserFindId';
 import UserFindPwd from './screen/UserFindPwd';
 import PostRegist from './screen/PostRegist';
 import RentalItem from "./screen/RentalItem";
-//dlfjsfldjs dnjfdydlfdl ektl ckwdkdhktek vlrhsgkek sksdho ekrxdmstlftnfmf
+//dlfjsfldjs dnjfdydlfdl ektl ckwdkdhktek vlrhsgkek sksdho ekrxdmstlftnf
 function App() {
   return (
     <ProjectProvider>
