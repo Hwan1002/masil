229b5a9694abf790a8eca0448801fbd172ad1788
dongugchoi <ehddnr7321@gmail.com>
2026-01-11 17:44:15 +0900

    등록일 대여일 가격 mb-0

--- masil-react/src/screen/SelectedRentalItem/components/PostInfo.js
+++ masil-react/src/screen/SelectedRentalItem/components/PostInfo.js
@@ -3,15 +3,15 @@ const PostInfo = ({ item, formatDate, formatCurrency }) => (
     <div className="selected-info">
       <div className="div-flex">
         <label>등록일 : </label>
-        <p>{formatDate(item.updateDate)}</p>
+        <p className="mb-0">{formatDate(item.updateDate)}</p>
       </div>
       <div className="div-flex">
         <label>대여일 : </label>
-        <p>{formatDate(item.postStartDate)}</p>
+        <p className="mb-0">{formatDate(item.postStartDate)}</p>
       </div>
       <div className="div-flex">
         <label>가격 : </label>
-        <p>{formatCurrency(item.postPrice)}원</p>
+        <p className="mb-0">{formatCurrency(item.postPrice)}원</p>
       </div>
       <div className="div-flex">
         <label>거래희망장소 : </label>
